c2ca0dac1f024dcb8aaaf44e5937d40402236888
sssin1236 <suj0358@gmail.com>
2021-09-17 16:05:59 +0900

    변수 설정해서 다시 코딩해보기

--- main.js
+++ main.js
@@ -11,9 +11,16 @@
 4. 맨 앞부분의 li를 뜯어서 제일 뒤로 보냄
 */
 
+const $list = $(".list");
+const $imgs = $list.children("li");
+const $prev = $(".prev");
+const $next = $(".next");
+
+let speed = 1000;
 let enableClick = true;
 let len = $(".list li").length;
 
+/*
 $(".list li").last().prependTo(".list");
 
 $(".list").css({
@@ -55,4 +62,48 @@ $(".prev").on("click", function(e){
         enableClick = false;
     }
     
+});
+*/
+
+$imgs.last().prependTo($list);
+
+$list.css({
+    width: 100 * len + "%",
+    height: "100%",
+    marginLeft: "-100%"
+});
+
+$imgs.css({
+    width: 100 / len +"%",
+    height: "100%",
+    float: "left"
+});
+
+$prev.on("click", function(e){
+    e.preventDefault();
+
+    if(enableClick){
+        $list.animate({ marginLeft : "0%" }, speed, function(){
+            $list.css({ marginLeft : "-100%" });
+            $list.children("li").last().prependTo($list);
+            enableClick = true;
+        });
+        enableClick = false;
+    }
+
+});
+
+
+$next.on("click", function(e){
+    e.preventDefault();
+
+    if(enableClick){
+        $list.animate({ marginLeft : "-200%" }, speed, function(){
+            $list.css({ marginLeft : "-100%" });
+            $list.children("li").first().appendTo($list);
+            enableClick = true;
+        });
+        enableClick = false;
+    }
+
 });
